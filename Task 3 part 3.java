db93f4576b2803f9dbeef3bb02607e48ce76cf30
YashG-J <yashgadhe80@gmail.com>
2026-01-03 20:26:58 +0530

    Task 3 part 3.java
    Task 3 part 3 . 
    To Execute Task 3 Run this part .

---
+++ Task 3 part 3.java
@@ -0,0 +1,12 @@
+package ATM;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        BankAccount userAccount = new BankAccount(5000);
+        User_interface atm = new User_interface(userAccount);
+
+        atm.menu();
+    }
+}
